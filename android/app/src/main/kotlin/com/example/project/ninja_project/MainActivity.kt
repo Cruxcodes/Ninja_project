0ce314e37c74e5451b421077beb91e4e96de9733
Abdullah Mudathir <abdullah.mudathir@isslng.com>
2023-05-04 22:52:05 +0100

    this is a ninja project

---
+++ android/app/src/main/kotlin/com/example/project/ninja_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project.ninja_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      appBar: AppBar(
+        title: Text("Ninja Id Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[700],
+        elevation: 50.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/picture.jpg'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 40.0,
+              color: Colors.white,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Ninja Name',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 29.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Current Ninja Level',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 29.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'cruxcodes@gmail.com',
+                  style: TextStyle(
+                      color: Colors.white38,
+                      fontSize: 18.0,
+                      letterSpacing: 1.0),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
